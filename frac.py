Fix fraction sign representation and result type on tableau

frac.py
@@ -26,6 +26,10 @@ class Fraction:
             if np.isclose(denominator, 0):
                 raise DivisionByZero
 
+            elif denominator < 0:
+                denominator = -denominator
+                self.numerator = -self.numerator
+
             denom_fraction = Fraction(*denominator.as_integer_ratio())
 
             self.numerator *= denom_fraction.denominator
@@ -34,6 +38,10 @@ class Fraction:
         elif np.issubdtype(type(denominator), np.integer):
             if denominator == 0:
                 raise DivisionByZero
+
+            elif denominator < 0:
+                denominator = -denominator
+                self.numerator = -self.numerator
 
             self.denominator *= denominator
 
@@ -105,4 +113,4 @@ class Fraction:
         return self.numerator / self.denominator
 
     def __repr__(self) -> str:
-        return f"{self.numerator}/{self.denominator}"
+        return f"{self.numerator:+}/{self.denominator}"

simplex.py
@@ -92,6 +92,8 @@ class FPI(PL):
         self.tableau[ 1 : , : self.num_vars ] = self.a_matrix
         self.tableau[ 1 : , -1 ] = self.b_vec.T
 
+        self.tableau[ 0, -1 ] = padronize(self.tableau[ 0, -1 ], self.fraction)
+
     def __init_c__ (self, pl: PL) -> None:
         self.c_vec = np.zeros(self.num_vars, dtype=pl.c_vec.dtype)
         self.c_vec[ : -self.num_aux] = (
@@ -135,10 +137,10 @@ class FPI(PL):
         st = "| " + " ".join([
             str_ratio(self.tableau[ 0, column ]) + ("*" if column == column_t else "")
             for column in range(self.tableau.shape[1] - 1)
-        ]) + f" | {str_ratio(self.tableau[ 0, -1 ])} |\n"
+        ]) + f" | {str_ratio(self.tableau[ 0, -1 ])} | \n"
 
         return st + "\n".join([
-            f"| {' '.join([ str_ratio(res) for res in self.tableau[ row, : -1 ] ] )} | " +
+            f"| {' '.join([ str_ratio(res) for res in self.tableau[ row, : -1 ] ] )} |" +
             f" {str_ratio(self.tableau[ row, -1 ])} | " + (str_ratio(min_t) if row_t == row else "")
             for row in range(1, self.num_res + 1)
         ]) + "\n"