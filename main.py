f6a1651a4747b64cf6c4d381df17c6b692234227
caiocaldeira3 <caio.caldeira3@gmail.com>
2022-05-29 21:54:27 -0300

    Clean input and update to use new printer

--- main.py
+++ main.py
@@ -1,24 +1,27 @@
 import numpy as np
+from util import WrongAnswer
 
 from simplex import PL
 
 if __name__ == "__main__":
-    num_res, num_vars = input("Número de váriaveis e número de restrições:\n").split(" ")
-    num_vars = int(num_vars)
-    num_res = int(num_res)
+    num_res, num_vars = map(int, input().split(" "))
 
-    c_vec = np.empty(num_vars)
     restrictions = np.empty((num_res, num_vars + 1))
-
-    c_str = input("C_vec\n").split(" ")
-    for indx in range(num_vars):
-        c_vec[indx] = float(c_str[indx])
+    c_vec =  np.fromiter(map(float, input().split(" ")), dtype=np.float32)
 
     for row in range(num_res):
-        row_str = input(f"{row}-th row: ").split(" ")
+        row_str = input().split(" ")
         for column in range(num_vars + 1):
             restrictions[row, column] = float(row_str[column])
 
-    pl = PL(c_vec, restrictions, fraction=False)
-    print(pl.solve())
+    try:
+        pl = PL(c_vec, restrictions, fraction=False)
+        res = pl.solve()
 
+        res.print(pl)
+
+    except WrongAnswer:
+        plf = PL(c_vec, restrictions, fraction=True)
+        res = plf.solve()
+
+        res.print(pl)
